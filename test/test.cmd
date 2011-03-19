79dbe218e017fb48cd5b0e0f087096c0d8f3f481
Duncan Smart <duncan.smart@gmail.com>
2011-03-19 19:40:51 +0000

    Moved tests to test dir

--- test/test.cmd
+++ test/test.cmd
@@ -1,6 +1,6 @@
 @echo off
 set TEST_FILE="%TEMP%\less-test-%RANDOM%.css"
-call lessc.cmd test.less > %TEST_FILE%
+call ..\lessc.cmd test.less > %TEST_FILE%
 echo n | comp test-expected.css %TEST_FILE% /A 2> nul
 if %ERRORLEVEL% EQU 0 (
     echo === PASS ===
